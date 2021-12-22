40a6ec1c1d35ddd2df0000c73c67e85899f36d46
Vanessa <interntemp21@gmail.com>
2021-12-22 17:35:27 +0800

    half

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,12 @@
 import { getFeaturedEvents } from "../dummy-date";
+import EventList from "../components/events/event-list";
 
 function HomePage() {
     const featureEvents = getFeaturedEvents();
 
     return (
         <div>
-            <ul>
-                <li></li>
-            </ul>
+            <EventList items={featureEvents}/>
         </div>
     );
 }
